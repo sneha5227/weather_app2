53af969bf5284989d613ad9ab829ecd255284493
snehayadav--demo <vst093963@gmail.com>
2026-01-30 14:05:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+// =========== Taking input ==========
+
+const inputElem = document.querySelector("input");
+const buttonElem = document.querySelector("#search");
+
+buttonElem.addEventListener("click", ()=> {
+    const location = inputElem.value.trim();
+
+    //call the API ->
+
+    fn9
+
+
+    //update the DOM
+fn90
+
+    inputElem.value="";
+})
