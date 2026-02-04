5f338c0f04f5f05727443e84ac8227a4274b079e
sneha yadav <vst093963@gmail.com>
2026-02-04 11:15:59 +0530

    Refactor weather fetching and DOM updating logic

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
+//Take input from user and print it onClick of the button.
 
+const searchInput = document.querySelector("input");
+const searchBtn = document.querySelector("#search");
 
-// =========== Taking input ==========
+searchBtn.addEventListener("click", () => {
+  // alert("I was clicked")
+  const location = searchInput.value;
+  //Call API
 
-const inputElem = document.querySelector("input");
-const buttonElem = document.querySelector("#search");
+  fetchWeather(location).then((data) => {
+    console.log("Data is ... ", data);
 
-buttonElem.addEventListener("click", ()=> {
-    const location = inputElem.value.trim();
+    //Update DOM
 
-    //call the API ->
+    updateDOM(data);
+  });
 
-    fn9
+  searchInput.value = ""; //
+});
 
+async function fetchWeather(location) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=c7236d36debb4636a18170654262201&q=${location}&aqi=no`;
+  const response = await fetch(url);
+  //   console.log(response);
 
-    //update the DOM
-fn90
+  //error handling
 
-    inputElem.value="";
-})
+  if (response.status === 400) {
+    //error logging location doesnt exist... //to-do
+    return error;
+  } else {
+    const jsonData = await response.json();
+    //   console.log(jsonData);
+
+    return jsonData;
+  }
+}
+
+function updateDOM(data) {
+  // ************1. filter required data*************
+//   console.log("I will update the dom ", data);
+const temp = data.current.temp_c;
+const location = data.location.name;
+const timeData = data.location.localtime;  //"2026-01-23 12:24"
+const [date, time] = timeData.split(" ");
+const iconUrl = data.current.condition.icon;
+
+console.log(temp,location,date,time,iconUrl);
+
+  // ************2. update the dom*************
+
+const temperatureEl = document.querySelector(".temperature");
+const locationEl = document.querySelector(".location");
+const iconImg = document.querySelector("img");
+
+temperatureEl.textContent = temp + " C";
+locationEl.textContent = location;
+iconImg.src = iconUrl;
+
+//update date-time etc
+
+}
